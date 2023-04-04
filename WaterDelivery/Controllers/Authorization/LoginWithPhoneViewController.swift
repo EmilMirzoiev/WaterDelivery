e9879a1ca0d0d5e18ab620bd702c65cf221cd0cc
Emil <emilmirzoiev@gmail.com>
2023-04-04 16:58:01 +0200

    changing error logic when providing phone

--- WaterDelivery/Controllers/Authorization/LoginWithPhoneViewController.swift
+++ WaterDelivery/Controllers/Authorization/LoginWithPhoneViewController.swift
@@ -32,12 +32,21 @@ class LoginWithPhoneViewController: BaseViewController {
     
     //Create an IBAction function for the sendSmsButton, which gets the text from the phoneNumberTextField and checks if it's not empty.
     @IBAction func sendSmsButtonTapped(_ sender: Any) {
-        guard let phoneNumber = phoneNumberTextField.text, phoneNumber != "" else {
+        guard let phoneNumber = phoneNumberTextField.text, !phoneNumber.isEmpty else {
+            errorLabel.text = "Please enter a phone number."
             errorLabel.isHidden = false
-//            showAlert(title: "Error", message: "Please enter correct number") {}
             return
         }
         
+        // Check if the phone number is in the correct format
+        let phoneRegex = "^\\+?[0-9]{7,16}$"
+        let phoneTest = NSPredicate(format: "SELF MATCHES %@", phoneRegex)
+        if !phoneTest.evaluate(with: phoneNumber) {
+            errorLabel.text = "Please enter a valid phone number."
+            errorLabel.isHidden = false
+            return
+        }
+
         //If it's not empty, it calls the startAuth function on the AuthManager singleton, passing in the phone number and a completion block that presents the PhoneSmsCodeViewController if the startAuth function is successful.
         AuthManager.shared.startAuth(phoneNumber: phoneNumber) { [weak self] success in
             if success {
@@ -45,7 +54,7 @@ class LoginWithPhoneViewController: BaseViewController {
             }
         }
     }
-
+    
     @IBAction func checkBoxTapped(_ sender: UIButton) {
         UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
             sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
