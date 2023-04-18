79763079902c907a65ab12df0d7f826d3821b1d0
Emil <emilmirzoiev@gmail.com>
2023-04-18 15:59:06 +0200

    Success + Failure VC added

---
+++ WaterDelivery/Controllers/Order+Payment/PaymentFailureViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentFailureViewController.swift
+//  WaterDelivery
+//
+//  Created by Emil on 18.04.23.
+//
+
+import UIKit
+
+class PaymentFailureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
---
+++ WaterDelivery/Controllers/Order+Payment/SuccessViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SuccessViewController.swift
+//  WaterDelivery
+//
+//  Created by Emil on 18.04.23.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
