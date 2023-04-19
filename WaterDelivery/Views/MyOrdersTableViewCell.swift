007c6efe27c13d9a4e4e1e2bd5be05f8e32cfbcb
Emil <emilmirzoiev@gmail.com>
2023-04-19 10:10:02 +0200

    cell modification

--- WaterDelivery/Views/MyOrdersTableViewCell.swift
+++ WaterDelivery/Views/MyOrdersTableViewCell.swift
@@ -15,31 +15,17 @@ class MyOrdersTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        layer.borderWidth = 1
+        layer.borderColor = AppColors.primary.cgColor
+        layer.cornerRadius = 24
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 8, bottom: 0, right: 8))
+        contentView.clipsToBounds = true
     }
-    
-//    //The cell class has a function "fill(with:)" that takes an "Order" object as a parameter. This function updates the text of the two labels with the values from the Order object.
-//    func fill(with model: Order) {
-//        orderDate.text = dateFormat(from: model.createdDate ?? Date())
-//        orderPrice.text = String(model.orderPrice ?? 0.0)
-//
-//        var productNameText = ""
-//        var productSizeText = ""
-//        var productAmountText = ""
-//
-//        for product in model.products {
-//            productNameText += "\(product.product.name ?? "")\n"
-//            productSizeText += "\(product.product.size ?? 0.0) -\n"
-//            productAmountText += "\(product.count) \n"
-//        }
-//
-//        productName.text = productNameText
-//        productSize.text = productSizeText
-//        productAmount.text = productAmountText
-//    }
     
     func fill(with model: Order) {
         orderDate.text = dateFormat(from: model.createdDate ?? Date())
-        orderPrice.text = String(model.orderPrice ?? 0.0)
+        orderPrice.text = "€ \(String(describing: model.orderPrice ?? 0.0))"
         
         var productsText = ""
         for product in model.products {
@@ -53,12 +39,19 @@ class MyOrdersTableViewCell: UITableViewCell {
         let attributedString = NSMutableAttributedString(string: productsText)
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 8
-        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
-        
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle,
+                                      value: paragraphStyle,
+                                      range: NSMakeRange(0, attributedString.length))
         productName.attributedText = attributedString
+        
+        // calculate the required height of the label based on its content
+        let size = productName.sizeThatFits(CGSize(width: productName.frame.size.width,
+                                                   height: CGFloat.greatestFiniteMagnitude))
+        productName.frame.size.height = size.height
     }
     
-    //The cell class also has a "dateFormat(from:)" function that takes a Date object as a parameter, formats it using a DateFormatter, and returns a string representation of the formatted date.
+    //The cell class also has a "dateFormat(from:)" function that takes a Date object as a parameter,
+    //formats it using a DateFormatter, and returns a string representation of the formatted date.
     func dateFormat(from model: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MM-dd-yyyy HH:mm"
