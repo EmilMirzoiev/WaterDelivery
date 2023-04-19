5289398aebb04a9341fc503f97e4560fc52c52fe
Emil <emilmirzoiev@gmail.com>
2023-04-19 12:40:20 +0200

    OrderDetailsViewController refactoring

--- WaterDelivery/Controllers/Order+Payment/OrderDetailsViewController.swift
+++ WaterDelivery/Controllers/Order+Payment/OrderDetailsViewController.swift
@@ -8,7 +8,9 @@
 import UIKit
 import FirebaseAuth
 
-class OrderDetailsViewController: BaseViewController, UITableViewDelegate, UITableViewDataSource {
+class OrderDetailsViewController: BaseViewController,
+                                  UITableViewDelegate,
+                                  UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var bottomView: UIView!
@@ -18,29 +20,28 @@ class OrderDetailsViewController: BaseViewController, UITableViewDelegate, UITab
     @IBOutlet weak var dateOfPriceLabel: UILabel!
     @IBOutlet weak var repeatOrderButton: UIButton!
     
-    //Create a variable named "order" of type "Order" to store the order data.
     var order: Order?
     
-    //Implement the viewDidLoad() method to set the data source and delegate for the tableView, register the table view cell, and reload the data.
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.register(.init(nibName: "BasketTableViewCell", bundle: nil), forCellReuseIdentifier: "BasketTableViewCell")
+        tableView.register(.init(nibName: "BasketTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "BasketTableViewCell")
         tableView.reloadData()
         updateUI()
     }
     
-    //Implement the updateUI() method to update the UI elements with the order data.
     func updateUI() {
         guard let order = order else { return }
         dateLabel.text = dateFormat(from: order.createdDate ?? Date())
         priceLabel.text = "\(order.orderPrice ?? 0.0)"
-        repeatOrderButton.layer.cornerRadius = min(repeatOrderButton.frame.size.width, repeatOrderButton.frame.size.height) / 2.0
+        repeatOrderButton.layer.cornerRadius =
+        min(repeatOrderButton.frame.size.width,
+            repeatOrderButton.frame.size.height) / 2.0
         repeatOrderButton.layer.masksToBounds = true
     }
     
-    //Implement the dateFormat(from:) method to format the date into a desired format.
     func dateFormat(from model: Date) -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = "MM-dd-yyyy HH:mm"
@@ -48,14 +49,16 @@ class OrderDetailsViewController: BaseViewController, UITableViewDelegate, UITab
         return formattedDateInString
     }
     
-    //Implement the required tableView delegate methods such as numberOfRowsInSection and cellForRowAt.
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
         order?.products.count ?? 0
     }
     
-    //In the cellForRowAt method, fill the table view cell with the order data and hide the plus and minus product buttons.
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "BasketTableViewCell", for: indexPath) as! BasketTableViewCell
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: "BasketTableViewCell", for: indexPath)
+        as! BasketTableViewCell
         
         if let order = order {
             cell.fill(with: order.products[indexPath.row])
@@ -64,8 +67,7 @@ class OrderDetailsViewController: BaseViewController, UITableViewDelegate, UITab
         cell.minusButton.isHidden = true
         return cell
     }
-    
-    //Implement the prepare(for:sender:) method to pass the basket data to the BasketViewController.
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as? BasketViewController,
            let order = order {
