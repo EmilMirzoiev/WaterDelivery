d9d55d94b9c2c70220a8cac81abe8c33c846d3a9
Emil <emilmirzoiev@gmail.com>
2023-04-19 10:51:12 +0200

    PaymentVC refactoring

--- WaterDelivery/Controllers/Order+Payment/PaymentViewController.swift
+++ WaterDelivery/Controllers/Order+Payment/PaymentViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 import Cloudipsp
 import FirebaseAuth
 
-class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFieldDelegate  {
+class PaymentViewController: BaseViewController,
+                             PSPayCallbackDelegate,
+                             UITextFieldDelegate  {
 
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
@@ -21,7 +23,6 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
     @IBOutlet weak var payButton: UIButton!
     @IBOutlet weak var cardCodeTextField: PSCVVTextField!
     
-    //Create a variable to store a reference to the PSCloudipspWKWebView, which is the web view used by the Cloudipsp API.
     var cloudipspWebView: PSCloudipspWKWebView!
     private var result: String?
     let userOrderManager = OrderManager()
@@ -32,8 +33,10 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
         getOrderPrice()
         prepareUI()
         
-        //Initialize the Cloudipsp web view and add it to the Payment View Controller's view hierarchy.
-        cloudipspWebView = PSCloudipspWKWebView(frame: CGRect(x: 0, y: 64, width: self.view.bounds.width, height: self.view.bounds.height))
+        cloudipspWebView = PSCloudipspWKWebView(
+            frame: CGRect(x: 0, y: 64,
+                          width: self.view.bounds.width,
+                          height: self.view.bounds.height))
         self.view.addSubview(cloudipspWebView)
     }
     
@@ -41,7 +44,9 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
         self.navigationItem.setHidesBackButton(true, animated: true)
         self.hideKeyboardWhenTappedAround()
         errorLabel.isHidden = true
-        payButton.layer.cornerRadius = min(payButton.frame.size.width, payButton.frame.size.height) / 2.0
+        payButton.layer.cornerRadius =
+        min(payButton.frame.size.width,
+            payButton.frame.size.height) / 2.0
         payButton.layer.masksToBounds = true
     }
     
@@ -50,23 +55,21 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
         return false
     }
     
-    //Add a method to retrieve the order price and display it in the price text field.
     func getOrderPrice() {
         let totalPrice = order.totalAmount
         priceLabel.text = "\(totalPrice)"
     }
     
-    //Add a method to clear the information in the text fields.
     func clearFieldsInfo() {
         cardNumberTextField.text = ""
         cardMonthTextField.text = ""
         cardYearTextField.text = ""
     }
     
-    //Implement the action method that will be called when the pay button is pressed. In the pay button action method, create an instance of the PSCloudipspApi class and use it to initiate a payment.
     @IBAction func onPayPressed(_ sender: Any) {
         let generatedOrderId = String(format: "Swift_%d", arc4random())
-        let cloudipspApi = PSCloudipspApi(merchant: 1397120, andCloudipspView: self.cloudipspWebView)
+        let cloudipspApi = PSCloudipspApi(merchant: 1397120,
+                                          andCloudipspView: self.cloudipspWebView)
         let card = self.cardInputLayout.confirm()
         if (card == nil) {
             showErrorMessage(with: "Please provide card number")
@@ -75,9 +78,12 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
             let amount = Double(priceLabel.text!)
             guard var amount = amount else { return }
             amount = amount * 100
-            let order = PSOrder(order: Int(amount), aStringCurrency: currencyLabel.text!,
-                                aIdentifier: generatedOrderId, aAbout: generatedOrderId)
-            cloudipspApi?.pay(card, with: order, andDelegate: self)
+            let order = PSOrder(order: Int(amount),
+                                aStringCurrency: currencyLabel.text!,
+                                aIdentifier: generatedOrderId,
+                                aAbout: generatedOrderId)
+            cloudipspApi?.pay(card, with: order,
+                              andDelegate: self)
         }
     }
     
@@ -85,7 +91,6 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
         self.cardInputLayout.test()
     }
     
-    //Implement the onPaidProcess method from the PSPayCallbackDelegate protocol to handle the results of the payment (e.g. to tell the user how went the payment and to add this order to orders list).
     func onPaidProcess(_ receipt: PSReceipt!) {
         debugPrint("onPaidProcess: %@", receipt.status)
         
@@ -95,7 +100,9 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
             clearFieldsInfo()
         } else {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
-                let failureVC = storyboard.instantiateViewController(withIdentifier: "FailureViewController") as! PaymentFailureViewController
+                let failureVC = storyboard.instantiateViewController(
+                    withIdentifier: "FailureViewController")
+                    as! PaymentFailureViewController
                 self.navigationController?.pushViewController(failureVC, animated: true)
         }
     }
@@ -116,7 +123,8 @@ class PaymentViewController: BaseViewController, PSPayCallbackDelegate, UITextFi
     
     func goToHomegape() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
-            let successVC = storyboard.instantiateViewController(withIdentifier: "SuccessViewController") as! SuccessViewController
+            let successVC = storyboard.instantiateViewController(
+                withIdentifier: "SuccessViewController") as! SuccessViewController
             self.navigationController?.pushViewController(successVC, animated: true)
     }
 
